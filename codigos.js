e4d9d17e59b969a039bac11ae23826ff78e2aca2
SZindustries <114617691+SZindustries@users.noreply.github.com>
2022-09-29 21:36:43 -0500

    Update codigos.js

--- codigos.js
+++ codigos.js
@@ -1,3 +1,3 @@
 let nombre = prompt("Como te llamas ?")
 
-alert( `Hola ${nombre} esta p[agina esta programada y diseñada por SZ Industry & M.Conce-TonyGODD`)
+alert( `Hola ${nombre} esta página esta programada y diseñada por SZ Industry & M.Conce-TonyGODD`)
